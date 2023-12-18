165e7bfe67be76b803b74fabe74d285ff4d1847c
Konstantinos Chondralis <kchondr@gmail.com>
2023-12-18 02:04:40 +0200

    remove utils file and merge Extractor subclasses into one single more generic extraction class

--- customer_extraction_strategy.py
+++ customer_extraction_strategy.py
@@ -1,48 +1,68 @@
 
-from typing import Type
+from typing import Set, Type
 from dream_candies import ExtractionStrategy
-from extractor import CustomerExtractor, InvoiceExtractor, InvoiceItemExtractor
+from extractor import Extractor
 
 class CustomerInvoicesExtractionStrategy(ExtractionStrategy):
     def __init__(self) -> None:
         super().__init__()
-        self.__extractor = None
+        self._extractor = Extractor()
 
     def implement(self):
-        # 1. sample customers
-        customer_sample = create_set(
+        customer_sample = self.create_set(
             filename='data/input/customer_sample.csv',
             key='CUSTOMER_CODE'
         )
 
-        customer_extractor = CustomerExtractor()
-        customer_extractor.extract(
-            samples=customer_sample,
+        self._extractor.extract(
+            lookup_set=customer_sample,
+            key='CUSTOMER_CODE',
             filename='data/input/customers.csv',
             output_file='data/output/customers.csv'
         )
 
-        invoices_extractor = InvoiceExtractor()
-        invoices_extractor.extract(
-            samples=customer_sample,
+        self._extractor.extract(
+            lookup_set=customer_sample,
+            key='CUSTOMER_CODE',
             filename='data/input/invoice.csv',
             output_file='data/input/invoice.csv'
         )
 
-        invoices = create_set(
+        invoices = self.create_set(
             filename='data/input/customer_sample.csv',
             key='INVOICE_CODE'
         )
 
-        invoice_items_extractor = InvoiceItemExtractor()
-        invoice_items_extractor.extract(
-            samples=invoices,
+        self._extractor.extract(
+            lookup_set=invoices,
+            key='INVOICE_CODE',
             filename='data/input/invoice_items.csv',
             output_file='data/output/invoice_items.csv'
         )
 
         print("CustomerInvoicesExtractionStrategy")
-        pass
     
-    def sample_customers(self):
-        pass
+    def create_set(filename: str, key: str) -> Set[str]:
+        """
+        Creates a set from a `.csv` file containing the values under `key` column
+        """
+        output = set()
+        with open(filename, 'r') as file:
+            columns = file.readline()
+            columns = columns.replace('"', '') \
+                            .strip() \
+                            .split(',')
+            try:
+                index_of_key = columns.index(key)
+            except ValueError as e:
+                print(e)
+                print(f" ERROR: key {key} not found in {filename} columns ")
+                return 1
+
+            for line in file:
+                line = line.replace('"', '') \
+                            .strip() \
+                            .split(',')
+                output.add(line[index_of_key])
+        
+        return output
--- extractor.py
+++ extractor.py
@@ -1,5 +1,4 @@
 from typing import Set
-from utils import read_lines_from, to_dict
 from exporter import Exporter
 
 
@@ -7,29 +6,25 @@ class Extractor:
     def __init__(self) -> None:
         pass
     
-    def extract(filename: str, output_path: str):
-        pass
+    def extract(
+        lookup_set: Set[str],
+        key: str,
+        filename: str,
+        output_file: str
+    ):
+        with open(filename, 'r') as file:
+            columns = file.readline()
+            columns = columns.replace('"', '') \
+                        .strip() \
+                        .split(',')
+            try:
+                index_of_key = columns.index(key)
+            except ValueError as e:
+                print(e)
+                print(f" ERROR: key {key} not found in {filename} columns ")
+                return 1
+            
+            for line in file:
+                if line[index_of_key] in lookup_set:
+                    Exporter.export(line, output_file)
 
-class CustomerExtractor(Extractor):
-    """Extract customer data using customer_sample"""
-    def extract(lookup_set: Set[str], filename: str, output_file):
-        for line in read_lines_from(filename):
-            customer_reccord = to_dict(line)
-            if customer_reccord['CUSTOMER_CODE'] in lookup_set:
-                Exporter.export(customer_reccord, output_file)
-
-class InvoiceExtractor(Extractor):
-    """Extract invoices data using customer_sample"""
-    def extract(lookup_set: Set[str], filename: str, output_file):
-        for line in read_lines_from(filename):
-            invoice_reccord = to_dict(line)
-            if invoice_reccord['CUSTOMER_CODE'] in lookup_set:
-                Exporter.export(invoice_reccord, output_file)
-    
-class InvoiceItemExtractor(Extractor):
-    """Extract invoices items using invoices sapmle"""
-    def extract(lookup_set: Set[str], filename: str, output_file):
-        for line in read_lines_from(filename):
-            invoice_item_reccord = to_dict(line)
-            if invoice_item_reccord['CUSTOMER_CODE'] in lookup_set:
-                Exporter.export(invoice_item_reccord, output_file)
